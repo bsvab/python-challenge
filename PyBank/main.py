9aa13128ffa0c1c47578fb6e6bbf3fbfba95b0ee
bsvab <bnsvab@gmail.com>
2023-11-15 01:42:11 -0600

    Tuesday work commit
    PyPoll code has both original code and brainstorming section at end to make it less repetitive/shorter - both work! Committing this so both are saved in the archive. Will do a final commit tomorrow cleaning it up.

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,81 +1,71 @@
+import os
+import csv
 
-# Your task is to create a Python script that analyzes the records to calculate each of the following values:
-#   The total number of months included in the dataset
-#   The net total amount of "Profit/Losses" over the entire period
-#   The changes in "Profit/Losses" over the entire period, and then the average of those changes
-#   The greatest increase in profits (date and amount) over the entire period
-#   The greatest decrease in profits (date and amount) over the entire period
-
-# this comment is typical for all "line_descriptor" variables in the following code - saving all lines to be eventually printed to the .txt file as 
-    # variables so I can use them both to print to terminal and to .txt without typing it out twice
+# this comment is typical for all "line_descriptor" variables in the following code:
+    # purpose is to save all lines to be eventually printed to the .txt file as variables so I can use them to print to terminal and to .txt without typing it out twice
 
 line_title = 'Financial Analysis'   
 line_break = '----------------------------'
 print(line_title)
 print(line_break)
 
-import os
-import csv
-
 csvpath = os.path.join('resources','budget_data.csv')
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    csvheader = next(csvreader)    # skip header row
+    next(csvreader)             # skip header row
 
-    # ====================================================================================================================
-    # code to return the total months, return the total profit/loss, and generate a dictionary of the profit/loss change values
+    # return the total months, return the total profit/loss, and generate lists of the dates and profit/loss change values...
 
-    total_profloss = 0  # establishing total profit/loss amount accumulator
-    row_count = 0   # establishing row (month) count accumulator
-    profloss_change = 0     # establishing variable for the profit/loss deltas to be populated into a dict in the for loop
+    total_profloss = 0          # establishing total profit/loss amount accumulator
+    row_count = 0               # establishing row (month) count accumulator
+    profloss_change = 0         # establishing variable for the profit/loss deltas to be populated into a dict in the for loop
     list_profloss_change = []   # establishing empty list to pull all the profit/loss change values into in order to feed into sum/min/max formulas later 
-    dict_profloss_change = {}   # establish empty dict to receive the profit/loss deltas and dates to use to pull the date associated w/ the min/max later
+    list_dates = []             # establishing empty list to pull all the date strings into in order to feed into min/max printout later 
 
     for row in csvreader:
-        if row_count >= 1:
-            profloss_change = int(row[1])-profloss  # calc profloss_change and apply to variable
-            list_profloss_change.append(profloss_change)    # populate list
-            dict_profloss_change["date"] = row[0]   # populate first key in dict
-            dict_profloss_change["pl-change"] = profloss_change     # populate second key in dict
-        profloss = int(row[1])  # apply profloss value to variable
-        total_profloss += profloss  # increase accumulator for total_profloss
-        row_count += 1  # increase accumulator for row (month) count
-
-    line_total_months = f'Total Months: {row_count}'     # 1 row = 1 month, so count of rows is the count of months
+        dates = row[0]
+        if row_count >= 1:                                      # conditional needed to skip the first value since there is nothing to calculate a delta from
+            profloss_change = int(row[1])-profloss              # calc profloss_change and apply to variable
+            list_profloss_change.append(profloss_change)        # populate list
+            list_dates.append(dates)
+        profloss = int(row[1])                                  # apply profloss value to variable
+        total_profloss += profloss                              # increase accumulator for total_profloss
+        row_count += 1                                          # increase accumulator for row (month) count
+    
+    line_total_months = f'Total Months: {row_count}'            # 1 row = 1 month, so count of rows is the count of months
     print(line_total_months)
     line_total_profloss = f'Total: ${total_profloss}'
     print(line_total_profloss)
 
-    # ====================================================================================================================
-    # code to return the average change
+    # return the average change...
 
-    avg_profloss = round((sum(list_profloss_change) / (row_count - 1)),2)
-    line_avg_profloss = f'Average Change: ${avg_profloss}'
-    print(line_avg_profloss)
+    avg_profloss_change = round((sum(list_profloss_change) / (row_count - 1)),2)   # calc average profit/loss change
+    
+    line_avg_profloss_change = f'Average Change: ${avg_profloss_change}'
+    print(line_avg_profloss_change)
 
-    # ====================================================================================================================
-    # code to return the greatest increase and greatest decrease in profits + associated dates
+    # return the greatest increase and greatest decrease in profits + associated dates
 
-    min_profloss = min(list_profloss_change)   # identify min change
-    min_profloss_date = [key for key in dict_profloss_change['pl-change'] == min_profloss]  # identify date associated w/ min change
+    min_profloss = min(list_profloss_change)                            # identify min change
+    min_profloss_index = list_profloss_change.index(min_profloss)       # identify min change index so can use to cross-reference same index in dates list
+    min_profloss_date = list_dates[min_profloss_index]                  # pull date corresponding to same index as min change value
 
-    max_profloss = max(list_profloss_change)   # identify max change
-    max_profloss_date = [key for key in dict_profloss_change['pl-change'] == max_profloss]  # identify date associated w/ max change
+    max_profloss = max(list_profloss_change)                            # identify max change
+    max_profloss_index = list_profloss_change.index(max_profloss)       # identify max change index so can use to cross-reference same index in dates list
+    max_profloss_date = list_dates[max_profloss_index]                  # pull date corresponding to same index as max change value
 
-    # print all the things...
     line_max_profloss = f'Greatest Increase in Profits: {max_profloss_date} (${max_profloss})'
     print(line_max_profloss)
     line_min_profloss = f'Greatest Decrease in Profits: {min_profloss_date} (${min_profloss})'
     print(line_min_profloss)
 
-# ====================================================================================================================
-# code to write outputs to text file in analysis folder
+# write outputs to text file in analysis folder...
 
 analysis_path = os.path.join('analysis','analysis.txt')
 
 lines = [line_title, '\n', line_break, '\n',
-             line_total_months, '\n', line_total_profloss, '\n', line_avg_profloss, '\n', 
+             line_total_months, '\n', line_total_profloss, '\n', line_avg_profloss_change, '\n', 
              line_max_profloss, '\n', line_min_profloss]
 
 with open(analysis_path,'w') as text:
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,13 @@
+import os
+import csv
 
-# Your task is to create a Python script that analyzes the votes and calculates each of the following values:
-#   The total number of votes cast
-#   A complete list of candidates who received votes
-#   The percentage of votes each candidate won
-#   The total number of votes each candidate won
-#   The winner of the election based on popular vote
+# this comment is typical for all "line_descriptor" variables in the following code:
+    # purpose is to save all lines to be eventually printed to the .txt file as variables so I can use them to print to terminal and to .txt without typing it out twice
 
 line_title = 'Election Results'
 line_break = '-------------------------'
 print(line_title)
 print(line_break)
-
-import os
-import csv
 
 csvpath = os.path.join('resources','election_data.csv')
 
@@ -20,22 +15,12 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csvheader = next(csvreader)    # skip header row
 
-    # ====================================================================================================================
-    # code to return the total votes
+    # return the total votes and return results by candidate...
 
-    row_count = len(list(csvreader))
-    line_total_votes = f'Total Votes: {row_count}'     # 1 row = 1 ballot/vote, so count of rows is the count of votes
-    print(line_total_votes)
-    print(line_break)
-
-    # ====================================================================================================================
-    # code to return the results by candidate
-
-    #////////// something is wrong with this section of code = everything returns 0's
-
-    candidate1 = "Charles Casper Stockham"
-    candidate2 = "Diana DeGette"
-    candidate3 = "Raymon Anthony Doane"
+    row_count = 0                            # establishing row (vote) count accumulator
+    candidate1 = "Charles Casper Stockham"   # first candidate identifier
+    candidate2 = "Diana DeGette"             # second candidate identifier
+    candidate3 = "Raymon Anthony Doane"      # third candidate identifier
     candidate1_counter = 0
     candidate2_counter = 0
     candidate3_counter = 0
@@ -49,10 +34,15 @@ with open(csvpath) as csvfile:
             candidate3_counter += 1
         else:
             print("There are more than 3 candidates in the data.")
+        row_count += 1
 
-    candidate1_percentage = round((candidate1_counter / row_count), 3)
-    candidate2_percentage = round((candidate2_counter / row_count), 3)
-    candidate3_percentage = round((candidate3_counter / row_count), 3)
+    line_total_votes = f'Total Votes: {row_count}'     # 1 row = 1 ballot/vote, so count of rows is the count of votes
+    print(line_total_votes)
+    print(line_break)
+
+    candidate1_percentage = round((candidate1_counter / row_count * 100), 3)
+    candidate2_percentage = round((candidate2_counter / row_count * 100), 3)
+    candidate3_percentage = round((candidate3_counter / row_count * 100), 3)
 
     line_candidate1 = f'{candidate1}: {candidate1_percentage}% ({candidate1_counter})'
     line_candidate2 = f'{candidate2}: {candidate2_percentage}% ({candidate2_counter})'
@@ -62,13 +52,9 @@ with open(csvpath) as csvfile:
     print(line_candidate3)
     print(line_break)
 
-    #\\\\\\\\\\
-
-    # ====================================================================================================================
-    # code to return the winning candidate
+    # return the winning candidate...
     
-    winning_percentage = min(candidate1_percentage, candidate2_percentage, candidate3_percentage)
-
+    winning_percentage = max(candidate1_percentage, candidate2_percentage, candidate3_percentage)
     if winning_percentage == candidate1_percentage:
         line_winner = f'Winner: {candidate1}'
         print(line_winner)
@@ -83,8 +69,7 @@ with open(csvpath) as csvfile:
     
     print(line_break)
 
-# ====================================================================================================================
-# code to write outputs to text file in analysis folder
+# write outputs to text file in analysis folder...
 
 analysis_path = os.path.join('analysis','analysis.txt')
 
@@ -96,3 +81,53 @@ lines = [line_title, '\n', line_break, '\n',
 with open(analysis_path,'w') as text:
     text.writelines(lines)
 
+
+# ====================================================================================================================
+# ABOVE CODE WORKS, BUT MAKING NEW SECTION TO BRAINSTORM WAYS TO CLEAN UP REPETITION IN RESULT GENERATING SECTION...
+# ====================================================================================================================
+
+
+# this comment is typical for all "line_descriptor" variables in the following code:
+    # purpose is to save all lines to be eventually printed to the .txt file as variables so I can use them to print to terminal and to .txt without typing it out twice
+
+line_title = 'Election Results'
+line_break = '-------------------------'
+print(line_title)
+print(line_break)
+
+csvpath = os.path.join('resources','election_data.csv')
+
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)     # skip header row
+
+    # return the total votes and results by candidate...
+
+    candidates = ["Charles Casper Stockham", "Diana DeGette", "Raymon Anthony Doane"]                                                                   # establishing list of all candidate names
+    votes = [1 if vote[2] == candidates[0] else 2 if vote[2] == candidates[1] else 3 if vote[2] == candidates[2] else 'error' for vote in csvreader]    # list comprehension w/ conditionals to assign 1/2/3 identifiers to ea candidate's votes and feed into list to be processed later; also captures errors if there were names in data other than the candidate list
+    total_votes = len(votes)                                                                                                                            # total votes identified via length of list established in line above
+    candidate_votes = [votes.count(1), votes.count(2), votes.count(3)]                                                                                  # make list of ea candidate's vote count using .count w/ 1/2/3 identifier
+    if votes.count('error') > 0:
+        print('vote counter error: data displayed is missing candidates')                                                                               # this isn't really needed given the data set received, but figured I'd practice adding in an error-checker
+    candidate_vote_percentages = [round(count / total_votes * 100, 3) for count in candidate_votes]                                                     # calculates percent of total vote by candidate and feeds into list
+
+    line_total_votes = f'Total Votes: {total_votes}'
+    print(line_total_votes)
+    print(line_break)
+    
+    line_candidate1 = f'{candidates[0]}: {candidate_vote_percentages[0]}% ({candidate_votes[0]})'
+    line_candidate2 = f'{candidates[1]}: {candidate_vote_percentages[1]}% ({candidate_votes[1]})'
+    line_candidate3 = f'{candidates[2]}: {candidate_vote_percentages[2]}% ({candidate_votes[2]})'
+    print(line_candidate1)
+    print(line_candidate2)
+    print(line_candidate3)
+    print(line_break)
+
+    # return the winning candidate...
+    
+    winning_percentage = max(candidate_vote_percentages)                         # winning percentage identified via finding the max value in the of list of candidate vote percentages
+    winner = candidates[candidate_vote_percentages.index(winning_percentage)]    # winner is pulled from candidates list by using the index of the winning percentage value in the percentages list
+
+    line_winner = f'Winner: {winner}'
+    print(line_winner)
+    print(line_break)
